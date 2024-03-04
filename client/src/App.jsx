6e89da40dd532a23b0c39a5a8b693a09b4930a7a
wicky <tanart.raemee@hotmail.com>
2024-03-04 22:48:27 +0700

    wait for login func

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import "./App.css";
 import Login from "./pages/login/Login";
 import SignUp from "./pages/signup/Signup";
+import Home from "./pages/home/Home";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -9,7 +10,7 @@ function App() {
   return (
     <>
       <div className="p-4 h-screen flex items-center justify-center ">
-        <SignUp />
+        <Home />
       </div>
     </>
   );
